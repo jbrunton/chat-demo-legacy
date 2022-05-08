3cfcb99ca311dc1489572f05ca9f10bb7c1441d3
John Brunton <john_brunton@live.co.uk>
2022-05-08 15:00:54 +0100

    add Pulumi

---
+++ pulumi/index.ts
@@ -0,0 +1,67 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as digitalocean from "@pulumi/digitalocean";
+import { AppSpecService } from "@pulumi/digitalocean/types/input";
+
+const stackName = pulumi.getStack();
+const appName = `chat-demo-${stackName}`;
+
+type Environment = "production" | "staging" | "development";
+
+const getEnvironment = (): Environment => {
+  switch (stackName) {
+    case 'production': return 'production';
+    case 'staging': return 'staging';
+  }
+  return 'development';
+}
+
+const environment = getEnvironment();
+
+const getDomainName = (): string => {
+  switch (environment) {
+    case 'production': return 'chat-demo.jbrunton-do.com';
+    case 'staging': return 'chat-demo.staging.jbrunton-do.com';
+    case 'development': return stackName === 'dev'
+      ? 'chat-demo.dev.agileplanning.io'
+      : `${stackName}.chat-demo.dev.agileplanning.io`;
+  }
+};
+
+const domainName = getDomainName();
+
+pulumi.log.info(`environment config: environment=${environment}, domain=${domainName}`);
+
+const app: AppSpecService = {
+  name: "app",
+  httpPort: 3000,
+  image: {
+    registry: "jbrunton",
+    registryType: "DOCKER_HUB",
+    repository: "chat-demo-app",
+    tag: "latest",
+  },
+  instanceCount: 1,
+  instanceSizeSlug: "basic-xxs",
+  routes: [{
+      path: "/",
+  }],
+};
+
+new digitalocean.App(appName, {
+  spec: {
+    name: appName,
+    region: "lon",
+    domainNames: [{
+        name: domainName,
+        zone: "jbrunton-do.com",
+        type: "PRIMARY"
+    }],
+    // databases: [{
+    //   engine: "PG",
+    //   name: "db",
+    //   production: false,
+    //   version: "12",
+    // }],
+    services: [app],
+  },
+});

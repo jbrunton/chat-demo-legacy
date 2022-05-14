91f355c43aa41081ebb3e50669662237dd2338d5
John Brunton <john_brunton@live.co.uk>
2022-05-14 22:34:50 +0100

    set tag name from branch

--- app/build.sh
+++ app/build.sh
@@ -2,5 +2,13 @@
 
 set -e
 
-docker build app -t jbrunton/demo-chat-app
-docker push jbrunton/demo-chat-app
+GIT_COMMIT=$(git rev-parse --short HEAD)
+GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+TAG=${TAG:-"$GIT_BRANCH-$GIT_COMMIT"}
+DOCKER_PUSH=${DOCKER_PUSH:-false}
+
+docker build app -t jbrunton/demo-chat-app:$TAG
+
+if [ "$DOCKER_PUSH" = true ] ; then
+  docker push jbrunton/demo-chat-app:$TAG
+fi

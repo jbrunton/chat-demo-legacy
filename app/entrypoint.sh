7c5d9f31be26ece673f368ef5b7379580b1f0791
John Brunton <john_brunton@live.co.uk>
2022-05-15 13:18:13 +0100

    add logging to entrypoint

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+echo "config: replacing NEXT_PUBLIC_DOMAIN with $NEXT_PUBLIC_DOMAIN"
 find /usr/src/app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#NEXT_PUBLIC_DOMAIN#$NEXT_PUBLIC_DOMAIN#g"
 
 exec "$@"

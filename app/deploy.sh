01f618b88b76427114b11750c3f21773761dcedc
John Brunton <john_brunton@live.co.uk>
2022-06-02 16:17:37 +0100

    ci: add semantic-release

--- app/deploy.sh
+++ app/deploy.sh
@@ -4,7 +4,6 @@ set -e
 
 GIT_COMMIT=$(git rev-parse --short HEAD)
 GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-export TAG="$GIT_BRANCH-$GIT_COMMIT"
 STACK_NAME=${STACK_NAME?}
 
 cd pulumi
---
+++ app/release.config.js
@@ -0,0 +1,54 @@
+/** @type {import('@types/semantic-release').Options} */
+const options = {
+  branches: ["semantic-release"],
+  
+  plugins: [
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        releaseRules: [
+          { type: "breaking", release: "major" },
+          { type: "feat", release: "minor" },
+          { type: "perf", release: "minor" },
+          { type: "build", release: "patch" },
+          { type: "chore", release: "patch" },
+          { type: "fix", release: "patch" },
+          { type: "refactor", release: "patch" },
+          { type: "ci", release: false },
+          { type: "docs", release: false },
+          { type: "style", release: false },
+          { type: "test", release: false },
+        ],
+      },
+    ],
+    "@semantic-release/release-notes-generator",
+    ["@semantic-release/npm", {
+      publish: false,
+    }],
+    ["@semantic-release/github", {
+      successComment: false,
+      releasedLabels: false,
+    }],
+    [
+      "@semantic-release-plus/docker",
+      {
+        name: "jbrunton/chat-demo-app"
+      }
+    ],
+    [
+      "@semantic-release/git",
+      {
+        assets: [
+          "package.json",
+          "package-lock.json",
+        ],
+        message: "chore(release): release ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+      }
+    ],
+    ["@semantic-release/exec", {
+      successCmd: 'echo "::set-output name=VERSION::${nextRelease.version}"'
+    }],
+  ],
+};
+
+module.exports = options;

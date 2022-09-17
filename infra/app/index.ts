a1b8320bf306164e5db1f973ceeb5b1384c31ce3
John Brunton <john_brunton@live.co.uk>
2022-09-17 18:15:35 +0100

    extract shared lb

--- infra/app/index.ts
+++ infra/app/index.ts
@@ -6,9 +6,10 @@ const provider = new aws.Provider("aws", { region: "eu-west-2" });
 
 // const vpc = awsx.ec2.Vpc.getDefault({ provider });
 
-// const shared = new pulumi.StackReference("jbrunton/chat-demo-shared-infra/dev");
-// const loadBalancerArn = shared.getOutput("loadBalancerArn");
-// const clusterName = shared.getOutput("clusterName");
+const shared = new pulumi.StackReference("jbrunton/chat-demo-shared-infra/dev");
+const loadBalancerArn = shared.getOutput("loadBalancerArn");
+const clusterName = shared.getOutput("clusterName");
+const securityGroupName = shared.getOutput("securityGroupName");
 
 // const sg = aws.ec2.getSecurityGroup({ id: "sg-ba8248c5" }, { provider });
 
@@ -24,7 +25,7 @@ const provider = new aws.Provider("aws", { region: "eu-west-2" });
 //     vpcId: vpc.id,
 //     targetType: "ip"
 //   }, { provider });
-  
+
 //   const webListener = new aws.lb.Listener("chat-demo-http", {
 //     defaultActions: [{
 //       type: "forward",
@@ -84,8 +85,6 @@ const provider = new aws.Provider("aws", { region: "eu-west-2" });
 //   // })
 // });
 
-
-
 // // const targetGroupWeb = new awsx.lb.ApplicationTargetGroup("chat-demo-web", {
 // //   port: 80,
 // //   protocol: "HTTP",
@@ -93,122 +92,156 @@ const provider = new aws.Provider("aws", { region: "eu-west-2" });
 // //   loadBalancer: loadBalancer
 // // }, { provider });
 
-
-// create a cluster
-const cluster = new aws.ecs.Cluster("example");
-
-// define the default vpc info to deploy
 const vpc = aws.ec2.getVpcOutput({ default: true }, { provider });
-const subnets = aws.ec2.getSubnetsOutput({
-  filters: [
-    {
-      name: "vpc-id",
-      values: [vpc.id],
-    },
-  ],
-}, { provider });
-
-// create the security groups
-const securityGroup = new aws.ec2.SecurityGroup("example", {
-  vpcId: vpc.id,
-  description: "HTTP access",
-  ingress: [
-    {
-      protocol: "tcp",
-      fromPort: 80,
-      toPort: 80,
-      cidrBlocks: ["0.0.0.0/0"],
-    },
-  ],
-  egress: [
-    {
-      protocol: "-1",
-      fromPort: 0,
-      toPort: 0,
-      cidrBlocks: ["0.0.0.0/0"],
-    },
-  ],
-});
-
-// define a loadbalancer
-const lb = new aws.lb.LoadBalancer("example", {
-  securityGroups: [securityGroup.id],
-  subnets: subnets.ids,
-});
-
-// target group for port 80
-const targetGroupA = new aws.lb.TargetGroup("example", {
-  port: 80,
-  protocol: "HTTP",
-  targetType: "ip",
-  vpcId: vpc.id,
-});
-
-// listener for port 80
-const listenerA = new aws.lb.Listener("example", {
-  loadBalancerArn: lb.arn,
-  port: 80,
-  defaultActions: [
-    {
-      type: "forward",
-      targetGroupArn: targetGroupA.arn,
-    },
-  ],
-});
-
-const role = new aws.iam.Role("example", {
-  assumeRolePolicy: aws.iam.assumeRolePolicyForPrincipal({
-    Service: "ecs-tasks.amazonaws.com",
-  }),
-});
-
-new aws.iam.RolePolicyAttachment("example", {
-  role: role.name,
-  policyArn:
-    "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy",
-});
-
-const taskDefinition = new aws.ecs.TaskDefinition("example", {
-  family: "exampleA",
-  cpu: "256",
-  memory: "512",
-  networkMode: "awsvpc",
-  requiresCompatibilities: ["FARGATE"],
-  executionRoleArn: role.arn,
-  containerDefinitions: pulumi.all([securityGroup]).apply(([sg]) =>
-    JSON.stringify([
+const subnets = aws.ec2.getSubnetsOutput(
+  {
+    filters: [
       {
-        name: "my-app",
-        image: "nginx",
-        portMappings: [
-          {
-            containerPort: 80,
-            hostPort: 80,
-            protocol: sg.ingress[0].protocol,
-          },
-        ],
+        name: "vpc-id",
+        values: [vpc.id],
       },
-    ])
-  ),
-});
-
-const svcA = new aws.ecs.Service("example", {
-  cluster: cluster.arn,
-  desiredCount: 1,
-  launchType: "FARGATE",
-  taskDefinition: taskDefinition.arn,
-  networkConfiguration: {
-    assignPublicIp: true,
-    subnets: subnets.ids,
-    securityGroups: [securityGroup.id],
+    ],
   },
-  loadBalancers: [
-    {
-      targetGroupArn: targetGroupA.arn,
-      containerName: "my-app",
-      containerPort: 80,
-    },
-  ],
-});
+  { provider }
+);
 
-export const url = lb.dnsName;
+const result = pulumi
+  .all([loadBalancerArn, clusterName, securityGroupName])
+  .apply(([loadBalancerArn, clusterName, securityGroupName]) => {
+    pulumi.log.info(
+      `stack outputs: ${JSON.stringify(
+        { loadBalancerArn, clusterName },
+        null,
+        " "
+      )}`
+    );
+    const lb = aws.lb.getLoadBalancer({ arn: loadBalancerArn }, { provider });
+
+    const cluster = aws.ecs.getCluster({ clusterName }, { provider });
+    const securityGroup = aws.ec2.getSecurityGroup({ name: securityGroupName });
+
+    return pulumi
+      .all([cluster, securityGroup, lb])
+      .apply(([cluster, securityGroup, lb]) => {
+        // // create a cluster
+        // const cluster = new aws.ecs.Cluster("example");
+
+        // define the default vpc info to deploy
+        // const vpc = aws.ec2.getVpcOutput({ default: true }, { provider });
+        // const subnets = aws.ec2.getSubnetsOutput({
+        //   filters: [
+        //     {
+        //       name: "vpc-id",
+        //       values: [vpc.id],
+        //     },
+        //   ],
+        // }, { provider });
+
+        // create the security groups
+        // const securityGroup = new aws.ec2.SecurityGroup("example", {
+        //   vpcId: vpc.id,
+        //   description: "HTTP access",
+        //   ingress: [
+        //     {
+        //       protocol: "tcp",
+        //       fromPort: 80,
+        //       toPort: 80,
+        //       cidrBlocks: ["0.0.0.0/0"],
+        //     },
+        //   ],
+        //   egress: [
+        //     {
+        //       protocol: "-1",
+        //       fromPort: 0,
+        //       toPort: 0,
+        //       cidrBlocks: ["0.0.0.0/0"],
+        //     },
+        //   ],
+        // });
+
+        // define a loadbalancer
+        // const lb = new aws.lb.LoadBalancer("example", {
+        //   securityGroups: [securityGroup.id],
+        //   subnets: subnets.ids,
+        // });
+
+        // target group for port 80
+        const targetGroupA = new aws.lb.TargetGroup("example", {
+          port: 80,
+          protocol: "HTTP",
+          targetType: "ip",
+          vpcId: vpc.id,
+        });
+
+        // listener for port 80
+        const listenerA = new aws.lb.Listener("example", {
+          loadBalancerArn: lb.arn,
+          port: 80,
+          defaultActions: [
+            {
+              type: "forward",
+              targetGroupArn: targetGroupA.arn,
+            },
+          ],
+        });
+
+        const role = new aws.iam.Role("example", {
+          assumeRolePolicy: aws.iam.assumeRolePolicyForPrincipal({
+            Service: "ecs-tasks.amazonaws.com",
+          }),
+        });
+
+        new aws.iam.RolePolicyAttachment("example", {
+          role: role.name,
+          policyArn:
+            "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy",
+        });
+
+        const taskDefinition = new aws.ecs.TaskDefinition("example", {
+          family: "exampleA",
+          cpu: "256",
+          memory: "512",
+          networkMode: "awsvpc",
+          requiresCompatibilities: ["FARGATE"],
+          executionRoleArn: role.arn,
+          containerDefinitions: JSON.stringify([
+            {
+              name: "my-app",
+              image: "nginx",
+              portMappings: [
+                {
+                  containerPort: 80,
+                  hostPort: 80,
+                  protocol: "tcp",
+                },
+              ],
+            },
+          ]),
+        });
+
+        const svcA = new aws.ecs.Service("example", {
+          cluster: cluster.arn,
+          desiredCount: 1,
+          launchType: "FARGATE",
+          taskDefinition: taskDefinition.arn,
+          networkConfiguration: {
+            assignPublicIp: true,
+            subnets: subnets.ids,
+            securityGroups: [securityGroup.id],
+          },
+          loadBalancers: [
+            {
+              targetGroupArn: targetGroupA.arn,
+              containerName: "my-app",
+              containerPort: 80,
+            },
+          ],
+        });
+
+        return {
+          dnsName: lb.dnsName
+        };
+      });
+  });
+
+export const url = result.dnsName;
--- infra/shared/index.ts
+++ infra/shared/index.ts
@@ -8,9 +8,15 @@ const cluster = new aws.ecs.Cluster("chat-demo", undefined, { provider });
 
 //const vpc = aws.ec2.getVpc();
 
-const vpc = awsx.ec2.Vpc.getDefault({ provider });
-
-const subnetIds = vpc.getSubnets("public").then(subnets => subnets.map(subnet => subnet.id));
+const vpc = aws.ec2.getVpcOutput({ default: true }, { provider });
+const subnets = aws.ec2.getSubnetsOutput({
+  filters: [
+    {
+      name: "vpc-id",
+      values: [vpc.id],
+    },
+  ],
+}, { provider });
 //const 
 
 // const subnet1 = new aws.ec2.Subnet("default_subnet_2a", {
@@ -27,14 +33,37 @@ const subnetIds = vpc.getSubnets("public").then(subnets => subnets.map(subnet =>
 //   cidrBlock: "10.0.1.0/16",
 // }, { provider });
 
+const securityGroup = new aws.ec2.SecurityGroup("example", {
+  vpcId: vpc.id,
+  description: "HTTP access",
+  ingress: [
+    {
+      protocol: "tcp",
+      fromPort: 80,
+      toPort: 80,
+      cidrBlocks: ["0.0.0.0/0"],
+    },
+  ],
+  egress: [
+    {
+      protocol: "-1",
+      fromPort: 0,
+      toPort: 0,
+      cidrBlocks: ["0.0.0.0/0"],
+    },
+  ],
+}, { provider });
+
 const loadBalancer = new aws.lb.LoadBalancer("chat-demo", {
   internal: false,
   loadBalancerType: "application",
-  subnets: subnetIds
+  subnets: subnets.ids,
+  securityGroups: [securityGroup.id],
   // securityGroups: [sg.id],
   // subnets: [subnet.id],
 }, { provider });
 
 export const loadBalancerArn = loadBalancer.arn;
 export const clusterName = cluster.name;
+export const securityGroupName = securityGroup.name;
 // export const listenerArn = listener.arn;

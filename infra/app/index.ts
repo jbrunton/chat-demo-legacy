3d63b689ae0be2c1b0d3bea792f25a3f7c3aad51
John Brunton <john_brunton@live.co.uk>
2022-09-17 12:39:14 +0100

    add app infra

---
+++ infra/app/index.ts
@@ -0,0 +1,6 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
+import * as awsx from "@pulumi/awsx";
+
+const provider = new aws.Provider("aws", { region: "eu-west-2" });
+

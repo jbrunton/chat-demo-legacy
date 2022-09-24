96e3b1ef72f3eef9e27b3ed285f24d2740ccacbd
John Brunton <john_brunton@live.co.uk>
2022-09-24 20:41:30 +0100

    feat: use parameter store for secrets

--- infra/app/.jest/setEnvVars.js
+++
@@ -1,4 +0,0 @@
-process.env.GOOGLE_CLIENT_ID = "test-google-client-id";
-process.env.GOOGLE_CLIENT_SECRET = "test-google-client-secret";
-process.env.NEXTAUTH_SECRET = "test-next-auth-secret";
-process.env.SENDGRID_API_KEY = "test-sendgrid-api-key";

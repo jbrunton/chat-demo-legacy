061767b1204fbd724da6984b8538086c4fda1910
John Brunton <john_brunton@live.co.uk>
2022-09-28 21:15:46 +0100

    wip: disable logz.io in dev

--- infra/app/app/aws/get-app-spec.ts
+++ infra/app/app/aws/get-app-spec.ts
@@ -6,7 +6,9 @@ export const getTaskDefinitionSpec = (
   executionRoleArn: string,
   logGroupName: string
 ): aws.ecs.TaskDefinitionArgs => {
-  const containerDefinitions: Record<string, unknown>[] = [getAppSpec(config)];
+  const containerDefinitions: Record<string, unknown>[] = [
+    getAppSpec(config, logGroupName),
+  ];
 
   if (config.environment !== "development") {
     containerDefinitions.push(getLogsRouterSpec(logGroupName));
@@ -26,78 +28,92 @@ export const getTaskDefinitionSpec = (
 const getParamArn = (name: string) =>
   `arn:aws:ssm:eu-west-2:030461922427:parameter/chat-demo/production/${name}`;
 
-const getAppSpec = (config: StackConfig) => ({
-  name: "chat-demo-app",
-  image: `jbrunton/chat-demo-app:${config.tag}`,
-  portMappings: [
-    {
-      containerPort: 80,
-      hostPort: 80,
-      protocol: "tcp",
-    },
-  ],
-  environment: [
-    {
-      name: "NEXT_PUBLIC_DOMAIN",
-      value: config.publicUrl,
-    },
-    {
-      name: "PORT",
-      value: "80",
-    },
-    {
-      name: "ENVIRONMENT_TYPE",
-      value: config.environment,
-    },
-    {
-      name: "TAG",
-      value: config.tag,
-    },
-    {
-      name: "NEXTAUTH_URL",
-      value: config.publicUrl,
-    },
-    {
-      name: "EMAIL_TRANSPORT",
-      value: "sendgrid",
-    },
-  ],
-  secrets: [
-    {
-      name: "GOOGLE_CLIENT_ID",
-      valueFrom: getParamArn("google-client-id"),
-    },
-    {
-      name: "GOOGLE_CLIENT_SECRET",
-      valueFrom: getParamArn("google-client-secret"),
-    },
-    {
-      name: "NEXTAUTH_SECRET",
-      valueFrom: getParamArn("next-auth-secret"),
-    },
-    {
-      name: "SENDGRID_API_KEY",
-      valueFrom: getParamArn("sendgrid-api-key"),
-    },
-  ],
-  logConfiguration: {
-    logDriver: "awsfirelens",
-    options: {
-      Name: "Http",
-      Host: "listener.logz.io",
-      Port: "8071",
-      tls: "on",
-      "tls.verify": "off",
-      Format: "json_lines",
-    },
-    secretOptions: [
+const getAppSpec = (config: StackConfig, logGroupName: string) => {
+  const logConfiguration =
+    config.environment === "development"
+      ? {
+          logDriver: "awslogs",
+          options: {
+            "awslogs-group": logGroupName,
+            "awslogs-region": "eu-west-2",
+            "awslogs-stream-prefix": "ecs",
+          },
+        }
+      : {
+          logDriver: "awsfirelens",
+          options: {
+            Name: "Http",
+            Host: "listener.logz.io",
+            Port: "8071",
+            tls: "on",
+            "tls.verify": "off",
+            Format: "json_lines",
+          },
+          secretOptions: [
+            {
+              name: "URI",
+              valueFrom: getParamArn("logz-io-uri"),
+            },
+          ],
+        };
+
+  return {
+    name: "chat-demo-app",
+    image: `jbrunton/chat-demo-app:${config.tag}`,
+    portMappings: [
       {
-        name: "URI",
-        valueFrom: getParamArn("logz-io-uri"),
+        containerPort: 80,
+        hostPort: 80,
+        protocol: "tcp",
       },
     ],
-  },
-});
+    environment: [
+      {
+        name: "NEXT_PUBLIC_DOMAIN",
+        value: config.publicUrl,
+      },
+      {
+        name: "PORT",
+        value: "80",
+      },
+      {
+        name: "ENVIRONMENT_TYPE",
+        value: config.environment,
+      },
+      {
+        name: "TAG",
+        value: config.tag,
+      },
+      {
+        name: "NEXTAUTH_URL",
+        value: config.publicUrl,
+      },
+      {
+        name: "EMAIL_TRANSPORT",
+        value: "sendgrid",
+      },
+    ],
+    secrets: [
+      {
+        name: "GOOGLE_CLIENT_ID",
+        valueFrom: getParamArn("google-client-id"),
+      },
+      {
+        name: "GOOGLE_CLIENT_SECRET",
+        valueFrom: getParamArn("google-client-secret"),
+      },
+      {
+        name: "NEXTAUTH_SECRET",
+        valueFrom: getParamArn("next-auth-secret"),
+      },
+      {
+        name: "SENDGRID_API_KEY",
+        valueFrom: getParamArn("sendgrid-api-key"),
+      },
+    ],
+    logConfiguration,
+  };
+};
 
 const getLogsRouterSpec = (logGroupName: string) => ({
   name: "logzio-log-router",
